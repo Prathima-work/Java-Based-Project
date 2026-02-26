5da6ae7e37856bb04c02a737f93da4ad1f28919f
Prathima <prathimaworks2.com>
2026-02-26 15:02:46 +0530

    newline added

--- src/main/java/com/example/twitterapp/TwitterAppApplication.java
+++ src/main/java/com/example/twitterapp/TwitterAppApplication.java
@@ -10,8 +10,8 @@ public class TwitterAppApplication {
 		SpringApplication.run(TwitterAppApplication.class, args);
 		System.out.println("Hello, Twitter App!");
 		System.out.println("This is a simple Twitter application built with Spring Boot.");
-     System.out.println("Feel free to explore and customize it to your needs!");
-
+		System.out.println("Feel free to explore and customize it to your needs!");
+		
 
 	}
 
